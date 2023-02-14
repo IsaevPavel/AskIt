392ecc7d8852cec4c388ce77235c05bd9fae06db
amalgama <isaev.pavelalex@gmail.com>
2023-02-14 19:27:11 +0300

    Lesson 5 complete

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,44 @@
+class QuestionsController < ApplicationController
+  before_action :set_question!, only: %i[edit update destroy show]
+
+  def edit
+  end
+  def update
+    if @question.update question_params
+      flash[:success] = "Question updated!"
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+    flash[:success] = "Question deleted!"
+  end
+  def show
+    @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
+  end
+  def index
+    @questions = Question.all
+  end
+  def new
+    @question = Question.new
+  end
+  def create
+    @question = Question.new question_params
+    if @question.save
+      flash[:success] = "Question created!"
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+  def set_question!
+    @question = Question.find params[:id]
+  end
+end
